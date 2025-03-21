58c845d880d5b05e184f575dbfbcc0a80947f8b4
Marius Facktor <mariusfacktor@gmail.com>
2025-03-20 21:02:52 -0700

    made flexbox fit whole screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -7,6 +7,7 @@ import Listbox from 'primevue/listbox';
 import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Message from 'primevue/message';
+import Select from 'primevue/select';
 
 import axios from 'axios';
 
@@ -19,6 +20,7 @@ export default {
     InputText,
     Button,
     Message,
+    Select,
   },
 
   data() {
@@ -40,6 +42,7 @@ export default {
   },
 
   mounted() {
+    document.title = 'Market Simulator'; // set site title
     this.getPeople();
   },
 
@@ -223,121 +226,104 @@ export default {
 
 <template>
 
-  <!-- <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-  </header>
-  <br> -->
+
+  <body>
+
+    <div class="flexbox-container">
+      <div class="flexbox-item flexbox-item-1">
+
+        <h1 class="text-center text-3xl">Select Existing Person</h1>
 
 
-  <span>
-    <!-- <button @click="getMarket" class="button_getMarket">Get Market</button> -->
-    <Button type="submit" severity="secondary" label="Get Market" @click="getMarket" />
-    <div v-if="data_getMarket">
-      <pre>    {{ data_getMarket.message }}</pre>
+        <!-- <div v-if="data_getPeople">
+          <Listbox v-model="selectedPerson" :options="data_getPeople.data.people" filter class="w-full md:w-56" />
+          <p>Selected value: {{ selectedPerson }}</p>
+        </div> -->
+
+        <div v-if="data_getPeople">
+          <Select v-model="selectedPerson" :options="data_getPeople.data.people" placeholder="Select a Person" class="w-full md:w-56" filter/>
+          <p>Selected value: {{ selectedPerson }}</p>
+        </div>
+
+        <br>
+
+        <h1 class="text-center text-3xl">OR</h1>
+
+        <br>
+
+        <h1 class="text-center text-3xl">Create New Person</h1>
+
+        <div class="flex flex-col gap-2">
+          <InputText type="text" v-model="newPersonName" placeholder="Name" />
+          <Message size="small" severity="secondary" variant="simple"></Message>
+        </div>
+
+        <div v-if="newPersonName">
+          <Button type="submit" severity="secondary" label="Create New Person" @click="createPerson(newPersonName)" />
+        </div>
+
+
+      </div>
+
+      <div class="flexbox-item flexbox-item-2"></div>
+
+      <div class="flexbox-item flexbox-item-3">
+
+          <Button type="submit" severity="secondary" label="Get Market" @click="getMarket" />
+          <div v-if="data_getMarket">
+            <pre>    {{ data_getMarket.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+          <Button type="submit" severity="secondary" label="Get Price" @click="getPrice" />
+          <div v-if="data_getPrice">
+            <pre>    {{ data_getPrice.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+          <Button type="submit" severity="secondary" label="Get Assets" @click="getAssets" />
+          <div v-if="data_getAssets">
+            <pre>    {{ data_getAssets.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+          <Button type="submit" severity="secondary" label="Get People" @click="getPeople" />
+          <div v-if="data_getPeople">
+            <pre>    {{ data_getPeople.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+          <Button type="submit" severity="secondary" label="Create Person" @click="createPerson('Phillip Geeter')" />
+          <div v-if="data_createPerson">
+            <pre>    {{ data_createPerson.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+          <Button type="submit" severity="secondary" label="Sell" @click="sell" />
+          <div v-if="data_sell">
+            <pre>    {{ data_sell.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+          <Button type="submit" severity="secondary" label="Buy" @click="buy" />
+          <div v-if="data_buy">
+            <pre>    {{ data_buy.message }}</pre>
+          </div>
+          <div v-else><br></div>
+
+
+
+      </div>
+
     </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
 
-
-  <span>
-    <!-- <button @click="getPrice" class="button_getPrice">Get Price</button> -->
-    <Button type="submit" severity="secondary" label="Get Price" @click="getPrice" />
-    <div v-if="data_getPrice">
-      <pre>    {{ data_getPrice.message }}</pre>
-    </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
-
-
-  <span>
-    <!-- <button @click="getAssets" class="button_getAssets">Get Assets</button> -->
-    <Button type="submit" severity="secondary" label="Get Assets" @click="getAssets" />
-    <div v-if="data_getAssets">
-      <pre>    {{ data_getAssets.message }}</pre>
-    </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
-
-
-  <span>
-    <!-- <button @click="getPeople" class="button_getPeople">Get People</button> -->
-    <Button type="submit" severity="secondary" label="Get People" @click="getPeople" />
-    <div v-if="data_getPeople">
-      <pre>    {{ data_getPeople.message }}</pre>
-    </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
-
-
-  <span>
-    <!-- <button @click="createPerson" class="button_createPerson">Create Person</button> -->
-    <Button type="submit" severity="secondary" label="Create Person" @click="createPerson('Phillip Geeter')" />
-    <div v-if="data_createPerson">
-      <pre>    {{ data_createPerson.message }}</pre>
-    </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
-
-
-  <span>
-    <!-- <button @click="sell" class="button_sell">Sell</button> -->
-    <Button type="submit" severity="secondary" label="Sell" @click="sell" />
-    <div v-if="data_sell">
-      <pre>    {{ data_sell.message }}</pre>
-    </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
-
-
-  <span>
-    <!-- <button @click="buy" class="button_buy">Buy</button> -->
-    <Button type="submit" severity="secondary" label="Buy" @click="buy" />
-    <div v-if="data_buy">
-      <pre>    {{ data_buy.message }}</pre>
-    </div>
-    <div v-if="error">
-        <p>Error: {{ error }}</p>
-    </div>
-  </span>
-  <br>
-
-
-  <br>
-
-  <div v-if="data_getPeople">
-    <Listbox v-model="selectedPerson" :options="data_getPeople.data.people" filter class="w-full md:w-56" />
-    <p>Selected value: {{ selectedPerson }}</p>
-  </div>
-
-  <br>
-
-  <div class="flex flex-col gap-2">
-    <!-- <label for="username">Username</label> -->
-    <InputText type="text" v-model="newPersonName" placeholder="Name" />
-    <Message size="small" severity="secondary" variant="simple">Create a new person</Message>
-  </div>
-
-  <div v-if="newPersonName">
-    <Button type="submit" severity="secondary" label="Create New Person" @click="createPerson(newPersonName)" />
-  </div>
+  </body>
 
 
   
@@ -346,30 +332,32 @@ export default {
 
 
 <style scoped>
-header {
-  line-height: 1.5;
+
+
+.flexbox-container {
+  display: flex;
+  justify-content: space-around;
+  height: 100vh;
+  width: 90vw;
 }
 
-/*.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}*/
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  ./*logo {
-    margin: 0 2rem 0 0;
-  }*/
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.flexbox-item {
+  width: 300px;
+  margin: 8px;
+  border: 3px solid #333;
+  background-color: #dfdfdf;
 }
+
+.flexbox-item-1 {
+  flex-grow: 1;
+}
+
+.flexbox-item-2 {
+  flex-grow: 1;
+}
+
+.flexbox-item-3 {
+  flex-grow: 1;
+}
+
 </style>
